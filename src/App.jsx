45f34d05323270fd0f01fceeca137efc2dc1a686
binhzonnguyen <118323345+binhzonnguyen@users.noreply.github.com>
2024-07-05 21:26:42 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+function App() {
+  return (
+    <>
+      <NewExpense />
+      <Expenses />
+    </>
+  );
+}
+export default App;
